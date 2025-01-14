136799b4a1b615cbd67fe32aab145a792a7610c1
Youssef Amazzal <20youssefyoussef02@gmail.com>
2025-01-14 19:58:02 +0100

    implement Redis Service

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -2,12 +2,25 @@
 // Réponse :
 // Question: Quelles sont les bonnes pratiques pour les clés Redis ?
 // Réponse :
-
+const db = require('../config/db');
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
-  }
-  
-  module.exports = {
-    // TODO: Exporter les fonctions utilitaires
-  };
+  // TODO: Implémenter une fonction générique de cache
+  const client = db.getRedisClient();
+  await client.set(key, JSON.stringify(data), 'EX', ttl);
+}
+
+async function getCachedData(key) {
+  const client = db.getRedisClient();
+  const data = await client.get(key);
+  return data ? JSON.parse(data) : null;
+}
+
+async function deleteCachedData(key) {
+  const client = db.getRedisClient();
+  await client.del(key);
+}
+
+module.exports = {
+  // TODO: Exporter les fonctions utilitaires
+};
