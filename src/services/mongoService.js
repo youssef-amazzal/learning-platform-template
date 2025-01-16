d8ccbf6f27acb065b7b9736c27c1052e0aa38022
Youssef Amazzal <20youssefyoussef02@gmail.com>
2025-01-16 19:08:19 +0100

    fix mongo service

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -4,24 +4,28 @@
 const { ObjectId } = require('mongodb');
 const db = require('../config/db');
 
-// Fonctions utilitaires pour MongoDB
+// Convert a string ID to an ObjectId
+function toObjectId(id) {
+  return new ObjectId(id);
+}
+
+// Utility functions for MongoDB
 async function findOneById(collectionName, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
   if (typeof id !== 'string') throw new Error('ID must be a string');
 
-  const collection = db.getDb().collection(collectionName);
-  const result = await collection.findOne({ _id: new ObjectId(id) });
+  const collection = (await db.getDb()).collection(collectionName);
+  const result = await collection.findOne({ _id: toObjectId(id) });
   return result;
 }
 
 async function findAll(collectionName) {
-  const collection = db.getDb().collection(collectionName);
+  const collection = (await db.getDb()).collection(collectionName);
   const results = await collection.find({}).toArray();
   return results;
 }
 
 async function insertOne(collectionName, document) {
-  const collection = db.getDb().collection(collectionName);
+  const collection = (await db.getDb()).collection(collectionName);
   const result = await collection.insertOne(document);
   return result.ops[0];
 }
@@ -30,22 +34,22 @@ async function updateOneById(collectionName, id, update) {
   if (typeof id !== 'string') {
     throw new Error('ID must be a string');
   }
-  const collection = db.getDb().collection(collectionName);
-  const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: update });
+  const collection = (await db.getDb()).collection(collectionName);
+  const result = await collection.updateOne({ _id: toObjectId(id) }, { $set: update });
   return result.modifiedCount > 0;
 }
 
 async function deleteOneById(collectionName, id) {
   if (typeof id !== 'string') throw new Error('ID must be a string');
 
-  const collection = db.getDb().collection(collectionName);
-  const result = await collection.deleteOne({ _id: new ObjectId(id) });
+  const collection = (await db.getDb()).collection(collectionName);
+  const result = await collection.deleteOne({ _id: toObjectId(id) });
   return result.deletedCount > 0;
 }
 
-// Export des services
+// Export the services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  toObjectId,
   findOneById,
   findAll,
   insertOne,
